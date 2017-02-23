efb2e21a3893b855f2412bf1bf530049945f79a0
Lukasz Nawojowski <luknwpl@gmail.com>
2017-02-23 21:26:40 +0100

    Perform_epochs

--- perform_epochs.py
+++ perform_epochs.py
@@ -1,10 +1,13 @@
-def perform_epochs:
+def perform_epochs():
+    global videos, cache_servers, endpoints, population
+
+    videos, cache_servers, endpoints, = read()
     population = get_initial_population()
 
-    log(population)
+    # log(i, population)
     for i in range(start=0, stop=None):
         population = evolve(population)
-        save(best)
+        save(best_chromosome)
 
-        if i % log_interval == 0:
-            log(population)
+        # if i % log_interval == 0:
+        #     log(population)
