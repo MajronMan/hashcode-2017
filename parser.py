bf11aa9f02b2ec01224182ddc1e440b2ffead242
Majron Man <majron15@gmail.com>
2017-02-23 20:44:53 +0100

    little change to parser

--- parser.py
+++ parser.py
@@ -6,14 +6,11 @@ class Parser:
         v, e, r, c, x = list(map(int, input().split()))
         video_sizes = list(map(int, input().split())) # v numbers describing video sizes
         videos = [Video(video_sizes[i], i) for i in range(v)]
-        cache_servers  =[CacheServer(i, [], x) for i in range(c)]
-        datacenter = CacheServer(-1, [], -1)
+        cache_servers  =[CacheServer(i, {}, x) for i in range(c)]
         endpoints = []
         for i in range(e):
-            endpoints.append(Endpoint([], i, []))
             ld, k = list(map(int, input().split())) # latency to datacenter and number of cache servers
-            # id of datacenter is -1
-            endpoints[i].links.append(Link(ld, endpoints[i], datacenter))
+            endpoints.append(Endpoint([], i, [], ld))
             for j in range(k):
                 c, lc = list(map(int, input().split())) # id and latency to cache server
                 endpoints[i].links.append(Link(lc, endpoints[i], cache_servers[c]))
