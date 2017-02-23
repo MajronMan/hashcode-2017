cffc012860e3f053ba45284b145489353d82f165
mikeg19 <mkg.grabowski@gmail.com>
2017-02-23 19:34:22 +0100

    Main

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+"""
+Cellar Dwellers @ HashCode 2017
+"""
 
+
+def get_initial_population():
+    pass
+
+
+def get_unit():
+    pass
+
+
+def cost():
+    pass
+
+
+def rate_population():
+    pass
+
+
+def evolve():
+    pass
